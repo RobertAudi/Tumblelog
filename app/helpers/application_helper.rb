b9c6d0014440704d1efc58ba87a57602e929e209
Aziz Light <aziz@azizlight.me>
2011-09-07 18:16:58 +0200

    Added a format_date method to the ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
     end
   end
   
+  def format_date(date, include_time = false)
+    time = include_time ? " at %I:%M %p" : ""
+    date.strftime("%B #{date.day.ordinalize}, %Y#{time}")
+  end
+  
+  
 end
