6685c298e0cbd1dd5337be5203d7752bb7151278
Aziz Light <aziz@azizlight.me>
2011-09-15 23:56:21 +0200

    Updated the format_date helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,22 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
-  
+
   def format_date(date, include_time = false)
     time = include_time ? " at %I:%M %p" : ""
     date.strftime("%B #{date.day.ordinalize}, %Y#{time}")
   end
-  
-  
+
+  def format_date(date, format = "long", include_time = false)
+    if format == "long"
+      time = include_time ? " at %I:%M %p" : ""
+      date.strftime("%B #{date.day.ordinalize}, %Y#{time}")
+    elsif format == "short"
+      time = include_time ? " %I:%M %p" : ""
+      date.strftime("%D#{time}")
+    else
+      
+    end
+  end
+
 end
