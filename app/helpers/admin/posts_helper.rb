990a27e022548139b0dee57a861049512a852da5
Aziz Light <aziz@azizlight.me>
2011-09-26 22:11:22 +0100

    Better titles in the admin area

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -1,2 +1,17 @@
 module Admin::PostsHelper
+  def get_title(post)
+    if post.post_type == "text"
+      post.title
+    elsif post.post_type == "image"
+      "(no title)"
+    elsif post.post_type == "quote"
+      truncate(post.quote)
+    elsif post.post_type == "link"
+      truncate(post.link)
+    elsif post.post_type == "audio"
+      "(no title)"
+    elsif post.post_type == "video"
+      "(no title)"
+    end
+  end
 end
